b57f1d27f580b919afe35a6c6fb725df63bee1e2
will1008m <41597375+will1008m@users.noreply.github.com>
2021-04-27 10:44:06 -0500

    Update main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,12 +1,49 @@
+#include "thermoStates.h"
+#include "EncoderMonitor.h"
+#include <LiquidCrystal.h>
+#define INTERVAL 100
 
-float tempGoal; 
+unsigned long timer;
+float tempGoal = 100; 
+LiquidCrystal LcdDriver (A5, A4, 5, 6, 7, 8);
 
 void setup() {
   // put your setup code here, to run once:
-
+  LcdDriver.begin(16, 2);
+  LcdDriver.setCursor(0, 0);
+  timer = millis();      // Timer setup.
+  analogReference(INTERNAL);
+  EncoderInitialize();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  float measured;// Check time
+  String stateToString;
+  if (millis() - timer >= INTERVAL)     // if 500 milliseconds, compute voltage and temperature
+  {
+    float lastTemp = tempGoal;
+    measured = 1.1 * analogRead(0) / 10.24;
+    updateTemp(tempGoal, measured);
+    switch(state)
+    {
+      case STANDBY:
+        stateToString = "STANDBY";
+      case HEATING:
+        stateToString = "HEATING";
+      case COOLING:
+        stateToString = "COOLING";     
+    }
 
+    tempGoal = (encoderPosition / 16.0) + 70;
+    LcdDriver.clear();
+    LcdDriver.print(stateToString);
+    LcdDriver.print("   ");
+    LcdDriver.print(measured);
+    LcdDriver.setCursor(0, 1);
+    LcdDriver.print(tempGoal);
+    LcdDriver.setCursor(0, 0);
+    timer += INTERVAL;    
+  } // End of Timer
+  
 }
